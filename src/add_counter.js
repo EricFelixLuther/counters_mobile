ca8d8878ef68a9217df081ecf7f085f3c8071de2
Eric Luther <ericfelixluther@gmail.com>
2018-05-26 22:55:10 +0200

    New visuals with bootstrap. Fancy colors and stuff

--- src/add_counter.js
+++ src/add_counter.js
@@ -1,7 +1,21 @@
+var add = function(){
+  var placeholder = $(this).parent().prev().find("[placeholder='counter']");
+  placeholder.html(parseInt(placeholder.html()) + 1);
+}
+
+var substract = function(){
+  var placeholder = $(this).parent().next().find("[placeholder='counter']");
+  placeholder.html(parseInt(placeholder.html()) - 1);
+}
+
 $( document ).ready(function(){
-    $("#add_counter").click(function(){
-        $("#counter_container").append("<input type='text' />");
-        $("#counter_container").append("<input type='number' />");
-        $("#counter_container").append("<br />");
-    });
+  $("#add_counter").click(function(){
+    var new_counter = $("#hidden_tmp").children().clone();
+    new_counter.find("[placeholder='name']").html($("#new_name").val());
+    $("#counter_container").append(new_counter);
+    $("#counter_container").last().show();
+    $("button[operation='add']").click(add);
+    $("button[operation='substract']").click(substract);
+  });
+
 });
