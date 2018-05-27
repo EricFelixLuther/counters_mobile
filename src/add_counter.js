5748c6c1d22dcf680264b3b2dc0193769acde1e5
Eric Luther <ericfelixluther@gmail.com>
2018-05-27 14:34:55 +0200

    Fix for smallest screens. Fix for JS.

--- src/add_counter.js
+++ src/add_counter.js
@@ -12,10 +12,9 @@ $( document ).ready(function(){
   $("#add_counter").click(function(){
     var new_counter = $("#hidden_tmp").children().clone();
     new_counter.find("[placeholder='name']").html($("#new_name").val());
+    new_counter.find("button[operation='add']").click(add);
+    new_counter.find("button[operation='substract']").click(substract);
     $("#counter_container").append(new_counter);
-    $("#counter_container").last().show();
-    $("button[operation='add']").click(add);
-    $("button[operation='substract']").click(substract);
+    $("#counter_container").last(".row").show();
   });
-
 });
